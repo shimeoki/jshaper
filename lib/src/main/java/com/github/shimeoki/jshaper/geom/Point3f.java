ecd095958cd0548be2c784bd3b46adc980652196
shimeoki <shimeoki@gmail.com>
2024-11-17 23:32:58 +0300

    fix(Point3f): add parameters

--- lib/src/main/java/com/github/shimeoki/jshaper/geom/Point3f.java
+++ lib/src/main/java/com/github/shimeoki/jshaper/geom/Point3f.java
@@ -2,7 +2,7 @@ package com.github.shimeoki.jshaper.geom;
 
 public interface Point3f extends Point2f, Pos3f {
 
-    public void setZ();
+    public void setZ(final float z);
 
-    public void addZ();
+    public void addZ(final float dz);
 }
