2bf37ad855f60f2044977c0ba0605a4501a22aad
shimeoki <shimeoki@gmail.com>
2024-11-17 23:25:30 +0300

    feat(geom): add Point2f

---
+++ lib/src/main/java/com/github/shimeoki/jshaper/geom/Point2f.java
@@ -0,0 +1,12 @@
+package com.github.shimeoki.jshaper.geom;
+
+public interface Point2f extends Pos2f {
+
+    public void setX();
+
+    public void setY();
+
+    public void addX();
+
+    public void addY();
+}
