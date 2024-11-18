d86eee2cd0c475b6af2782ea6f1e597602f6ebf5
shimeoki <shimeoki@gmail.com>
2024-11-18 13:14:04 +0300

    refactor(Vector3f): remove Array2f reference

--- lib/src/main/java/com/github/shimeoki/jshaper/data/Vector3f.java
+++ lib/src/main/java/com/github/shimeoki/jshaper/data/Vector3f.java
@@ -2,42 +2,44 @@ package com.github.shimeoki.jshaper.data;
 
 public final class Vector3f implements Array3f {
 
-    private final Array2f array;
+    private float v1;
+    private float v2;
     private float v3;
 
     public Vector3f(final float v1, final float v2, final float v3) {
-        array = new Vector2f(v1, v2);
+        setValue1(v1);
+        setValue2(v2);
         setValue3(v3);
     }
 
     @Override
     public float value1() {
-        return array.value1();
+        return v1;
     }
 
     @Override
     public void setValue1(final float value) {
-        array.setValue1(value);
+        v1 = value;
     }
 
     @Override
     public void addToValue1(final float delta) {
-        array.addToValue1(delta);
+        setValue1(v1 + delta);
     }
 
     @Override
     public float value2() {
-        return array.value2();
+        return v2;
     }
 
     @Override
     public void setValue2(final float value) {
-        array.setValue2(value);
+        v2 = value;
     }
 
     @Override
     public void addToValue2(final float delta) {
-        array.addToValue2(delta);
+        setValue2(v2 + delta);
     }
 
     @Override
