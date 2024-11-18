a073f48b625ed44f9561c30badfc79fe7f68a985
shimeoki <shimeoki@gmail.com>
2024-11-18 12:05:21 +0300

    feat(data): add Array4f

---
+++ lib/src/main/java/com/github/shimeoki/jshaper/data/Array4f.java
@@ -0,0 +1,8 @@
+package com.github.shimeoki.jshaper.data;
+
+public interface Array4f extends Array3f, Tuple4f {
+
+    public void setValue4(final float value);
+
+    public void addToValue4(final float delta);
+}
