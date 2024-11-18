9266f1d01a26cb9626fdeaf6ee416cf0233d7256
shimeoki <shimeoki@gmail.com>
2024-11-18 12:02:30 +0300

    fix(Array2f): methods for value2

--- lib/src/main/java/com/github/shimeoki/jshaper/data/Array2f.java
+++ lib/src/main/java/com/github/shimeoki/jshaper/data/Array2f.java
@@ -5,4 +5,8 @@ public interface Array2f extends Tuple2f {
     public void setValue1(final float value);
 
     public void addToValue1(final float delta);
+
+    public void setValue2(final float value);
+
+    public void addToValue2(final float delta);
 }
