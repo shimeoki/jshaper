042560cf19b9b6ebb7a7ae895770324e87fb5ada
shimeoki <shimeoki@gmail.com>
2024-11-18 13:20:32 +0300

    refactor(Vector2f): make as an array

--- lib/src/main/java/com/github/shimeoki/jshaper/data/Vector2f.java
+++ lib/src/main/java/com/github/shimeoki/jshaper/data/Vector2f.java
@@ -2,8 +2,7 @@ package com.github.shimeoki.jshaper.data;
 
 public final class Vector2f implements Array2f {
 
-    private float v1;
-    private float v2;
+    private final float[] array = new float[2];
 
     public Vector2f(final float v1, final float v2) {
         setValue1(v1);
@@ -12,31 +11,31 @@ public final class Vector2f implements Array2f {
 
     @Override
     public float value1() {
-        return v1;
+        return array[0];
     }
 
     @Override
     public void setValue1(final float value) {
-        v1 = value;
+        array[0] = value;
     }
 
     @Override
     public void addToValue1(final float delta) {
-        setValue1(v1 + delta);
+        setValue1(value1() + delta);
     }
 
     @Override
     public float value2() {
-        return v2;
+        return array[1];
     }
 
     @Override
     public void setValue2(final float value) {
-        v2 = value;
+        array[1] = value;
     }
 
     @Override
     public void addToValue2(final float delta) {
-        setValue2(v2 + delta);
+        setValue2(value2() + delta);
     }
 }
