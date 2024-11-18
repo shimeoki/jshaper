382da8a9109d90a939b12e9a607e6ffdb3e26c70
shimeoki <shimeoki@gmail.com>
2024-11-18 12:00:53 +0300

    feat(data): add Array2f

---
+++ lib/src/main/java/com/github/shimeoki/jshaper/data/Array2f.java
@@ -0,0 +1,8 @@
+package com.github.shimeoki.jshaper.data;
+
+public interface Array2f extends Tuple2f {
+
+    public void setValue1(final float value);
+
+    public void addToValue1(final float delta);
+}
