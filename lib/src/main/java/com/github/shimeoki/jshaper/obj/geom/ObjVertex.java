12a87648363e92d77c49a69c560d5ed272c6314b
shimeoki <shimeoki@gmail.com>
2024-11-20 23:12:18 +0300

    feat(obj/geom): add ObjVertex

---
+++ lib/src/main/java/com/github/shimeoki/jshaper/obj/geom/ObjVertex.java
@@ -0,0 +1,23 @@
+package com.github.shimeoki.jshaper.obj.geom;
+
+import com.github.shimeoki.jshaper.data.Arrayf;
+import com.github.shimeoki.jshaper.geom.Pointf;
+
+public interface ObjVertex extends Pointf, Arrayf {
+
+    public float x();
+
+    public void setX(final float x);
+
+    public float y();
+
+    public void setY(final float y);
+
+    public float z();
+
+    public void setZ(final float z);
+
+    public float w();
+
+    public void setW(final float w);
+}
