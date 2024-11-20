bb7309060e2182ca6724472698fb41dc1de7fe66
shimeoki <shimeoki@gmail.com>
2024-11-21 02:18:21 +0300

    feat(ObjVertex)!: change to a class

--- lib/src/main/java/com/github/shimeoki/jshaper/obj/geom/ObjVertex.java
+++ lib/src/main/java/com/github/shimeoki/jshaper/obj/geom/ObjVertex.java
@@ -1,22 +1,86 @@
 package com.github.shimeoki.jshaper.obj.geom;
 
 import com.github.shimeoki.jshaper.geom.Pointf;
+import com.github.shimeoki.jshaper.geom.Pos;
 
-public interface ObjVertex extends Pointf {
+public final class ObjVertex implements Pointf {
 
-    public float x();
+    private final float[] values = new float[4];
 
-    public void setX(final float x);
+    public ObjVertex(final float x, final float y, final float z, final Float w) {
 
-    public float y();
+    }
 
-    public void setY(final float y);
+    @Override
+    public float[] array() {
+        return values;
+    }
 
-    public float z();
+    @Override
+    public float get(final Pos p) {
+        switch (p) {
+            case X:
+                return x();
+            case Y:
+                return y();
+            case Z:
+                return z();
+            case W:
+                return w();
+            default:
+                throw new IllegalArgumentException("ObjVertex: invalid Pos");
+        }
+    }
 
-    public void setZ(final float z);
+    @Override
+    public void set(final Pos p, final float value) {
+        switch (p) {
+            case X:
+                setX(value);
+                break;
+            case Y:
+                setY(value);
+                break;
+            case Z:
+                setZ(value);
+                break;
+            case W:
+                setW(value);
+                break;
+            default:
+                throw new IllegalArgumentException("ObjVertex: invalid Pos");
+        }
+    }
 
-    public float w();
+    public float x() {
+        return values[0];
+    }
 
-    public void setW(final float w);
+    public void setX(final float x) {
+        values[0] = x;
+    }
+
+    public float y() {
+        return values[1];
+    }
+
+    public void setY(final float y) {
+        values[1] = y;
+    }
+
+    public float z() {
+        return values[2];
+    }
+
+    public void setZ(final float z) {
+        values[2] = z;
+    }
+
+    public float w() {
+        return values[3];
+    }
+
+    public void setW(final float w) {
+        values[3] = w;
+    }
 }
