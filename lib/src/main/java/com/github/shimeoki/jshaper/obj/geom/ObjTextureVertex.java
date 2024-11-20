d86499848187cfc5c88ba8f5ef90a932ead0a741
shimeoki <shimeoki@gmail.com>
2024-11-21 02:23:46 +0300

    feat(ObjTextureVertex)!: change to a class

--- lib/src/main/java/com/github/shimeoki/jshaper/obj/geom/ObjTextureVertex.java
+++ lib/src/main/java/com/github/shimeoki/jshaper/obj/geom/ObjTextureVertex.java
@@ -1,18 +1,85 @@
 package com.github.shimeoki.jshaper.obj.geom;
 
 import com.github.shimeoki.jshaper.geom.Pointf;
+import com.github.shimeoki.jshaper.geom.Pos;
 
-public interface ObjTextureVertex extends Pointf {
+public final class ObjTextureVertex implements Pointf {
 
-    public float u();
+    private final float[] values = new float[3];
 
-    public void setU(final float u);
+    public ObjTextureVertex(final float u, final Float v, final Float w) {
+        setU(u);
 
-    public float v();
+        if (v != null) {
+            setV(v);
+        } else {
+            setV(0);
+        }
 
-    public void setV(final float v);
+        if (w != null) {
+            setW(w);
+        } else {
+            setW(0);
+        }
+    }
 
-    public float w();
+    @Override
+    public float[] array() {
+        return values;
+    }
 
-    public void setW(final float w);
+    @Override
+    public float get(final Pos p) {
+        switch (p) {
+            case U:
+                return u();
+            case V:
+                return v();
+            case W:
+                return w();
+            default:
+                throw new IllegalArgumentException("ObjTextureVertex: invalid Pos");
+        }
+    }
+
+    @Override
+    public void set(final Pos p, final float value) {
+        switch (p) {
+            case U:
+                setU(value);
+                break;
+            case V:
+                setV(value);
+                break;
+            case W:
+                setW(value);
+                break;
+            default:
+                throw new IllegalArgumentException("ObjTextureVertex: invalid Pos");
+        }
+    }
+
+    public float u() {
+        return values[0];
+    }
+
+    public void setU(final float u) {
+        values[0] = u;
+    }
+
+    public float v() {
+        return values[1];
+    }
+
+    public void setV(final float v) {
+        values[1] = v;
+    }
+
+    public float w() {
+        return values[2];
+    }
+
+    public void setW(final float w) {
+        values[2] = w;
+    }
 }
