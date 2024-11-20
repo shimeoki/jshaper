d658c0c90b428bc605f5e3ac0d8625440eb2e272
shimeoki <shimeoki@gmail.com>
2024-11-21 01:08:18 +0300

    feat(obj/data): add ObjTriplet

---
+++ lib/src/main/java/com/github/shimeoki/jshaper/obj/data/ObjTriplet.java
@@ -0,0 +1,10 @@
+package com.github.shimeoki.jshaper.obj.geom;
+
+public interface ObjTriplet {
+
+    public ObjVertex vertex();
+
+    public ObjTextureVertex textureVertex();
+
+    public ObjVertexNormal vertexNormal();
+}
