00b1860817d99efe08ed6790ef96bcf38668e6fb
shimeoki <shimeoki@gmail.com>
2024-11-21 02:44:57 +0300

    feat(ObjVertexData)!: change to a class

--- lib/src/main/java/com/github/shimeoki/jshaper/obj/data/ObjVertexData.java
+++ lib/src/main/java/com/github/shimeoki/jshaper/obj/data/ObjVertexData.java
@@ -1,19 +1,50 @@
 package com.github.shimeoki.jshaper.obj.data;
 
 import java.util.List;
+import java.util.Objects;
 
 import com.github.shimeoki.jshaper.obj.geom.ObjParameterSpaceVertex;
 import com.github.shimeoki.jshaper.obj.geom.ObjTextureVertex;
 import com.github.shimeoki.jshaper.obj.geom.ObjVertex;
 import com.github.shimeoki.jshaper.obj.geom.ObjVertexNormal;
 
-public interface ObjVertexData {
+public final class ObjVertexData {
 
-    public List<ObjVertex> vertices();
+    private final List<ObjVertex> vertices;
+    private final List<ObjTextureVertex> textureVertices;
+    private final List<ObjVertexNormal> vertexNormals;
+    private final List<ObjParameterSpaceVertex> parameterSpaceVertices;
 
-    public List<ObjTextureVertex> textureVertices();
+    public ObjVertexData(
+            final List<ObjVertex> vertices,
+            final List<ObjTextureVertex> textureVertices,
+            final List<ObjVertexNormal> vertexNormals,
+            final List<ObjParameterSpaceVertex> parameterSpaceVertices) {
 
-    public List<ObjVertexNormal> vertexNormals();
+        Objects.requireNonNull(vertices);
+        Objects.requireNonNull(textureVertices);
+        Objects.requireNonNull(vertexNormals);
+        Objects.requireNonNull(parameterSpaceVertices);
 
-    public List<ObjParameterSpaceVertex> parameterSpaceVertices();
+        this.vertices = vertices;
+        this.textureVertices = textureVertices;
+        this.vertexNormals = vertexNormals;
+        this.parameterSpaceVertices = parameterSpaceVertices;
+    }
+
+    public List<ObjVertex> vertices() {
+        return vertices;
+    }
+
+    public List<ObjTextureVertex> textureVertices() {
+        return textureVertices;
+    }
+
+    public List<ObjVertexNormal> vertexNormals() {
+        return vertexNormals;
+    }
+
+    public List<ObjParameterSpaceVertex> parameterSpaceVertices() {
+        return parameterSpaceVertices;
+    }
 }
