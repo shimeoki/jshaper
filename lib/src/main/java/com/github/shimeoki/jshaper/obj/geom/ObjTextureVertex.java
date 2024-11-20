909881cc1e1c12e44af20d837fac38e5f4d64fba
shimeoki <shimeoki@gmail.com>
2024-11-20 22:15:24 +0300

    feat(obj/geom): add ObjTextureVertex

---
+++ lib/src/main/java/com/github/shimeoki/jshaper/obj/geom/ObjTextureVertex.java
@@ -0,0 +1,7 @@
+package com.github.shimeoki.jshaper.obj.geom;
+
+import com.github.shimeoki.jshaper.data.Array3f;
+import com.github.shimeoki.jshaper.geom.PointUVW;
+
+public interface ObjTextureVertex extends Array3f, PointUVW {
+}
