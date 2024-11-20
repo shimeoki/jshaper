00b3a16ab3a609704e328ee9b7183139e53b9a4e
shimeoki <shimeoki@gmail.com>
2024-11-21 00:15:51 +0300

    feat(obj/geom): add ObjVertexNormal

---
+++ lib/src/main/java/com/github/shimeoki/jshaper/obj/geom/ObjVertexNormal.java
@@ -0,0 +1,18 @@
+package com.github.shimeoki.jshaper.obj.geom;
+
+import com.github.shimeoki.jshaper.geom.Pointf;
+
+public interface ObjVertexNormal extends Pointf {
+
+    public float i();
+
+    public void setI(final float i);
+
+    public float j();
+
+    public void setJ(final float j);
+
+    public float k();
+
+    public void setK(final float k);
+}
