668a1110ccdd4b5a38cec6ca2135a06ff3436d5b
shimeoki <shimeoki@gmail.com>
2024-11-23 09:53:04 +0300

    feat(obj/data): add ObjFile

---
+++ lib/src/main/java/com/github/shimeoki/jshaper/obj/data/ObjFile.java
@@ -0,0 +1,25 @@
+package com.github.shimeoki.jshaper.obj.data;
+
+import java.util.Objects;
+
+public final class ObjFile {
+
+    private final ObjVertexData vertexData;
+    private final ObjElements elements;
+
+    public ObjFile(final ObjVertexData vertexData, final ObjElements elements) {
+        Objects.requireNonNull(vertexData);
+        Objects.requireNonNull(elements);
+
+        this.vertexData = vertexData;
+        this.elements = elements;
+    }
+
+    public ObjVertexData vertexData() {
+        return vertexData;
+    }
+
+    public ObjElements elements() {
+        return elements;
+    }
+}
