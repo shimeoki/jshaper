5824904bcc4836e9110b235cd70e58d3fc326282
shimeoki <shimeoki@gmail.com>
2024-11-23 11:40:26 +0300

    feat(ObjModelReader): get BufferedReader

--- lib/src/main/java/com/github/shimeoki/jshaper/obj/reader/ObjModelReader.java
+++ lib/src/main/java/com/github/shimeoki/jshaper/obj/reader/ObjModelReader.java
@@ -1,6 +1,10 @@
 package com.github.shimeoki.jshaper.obj.reader;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import com.github.shimeoki.jshaper.obj.data.ObjFile;
 
@@ -10,6 +14,15 @@ public final class ObjModelReader implements ObjReader {
     public ObjFile read(final File f) throws ObjReaderException {
         if (!f.canRead()) {
             throw new ObjReaderException(ObjReaderExceptionType.IO, "file is not readable");
+        }
+
+        final Path p = f.toPath();
+
+        final BufferedReader r;
+        try {
+            r = Files.newBufferedReader(p);
+        } catch (IOException e) {
+            throw new ObjReaderException(ObjReaderExceptionType.IO, "error while opening the file");
         }
 
         // TODO
