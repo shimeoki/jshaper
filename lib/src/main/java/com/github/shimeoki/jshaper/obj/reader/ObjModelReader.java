ba40b8b3c232ee4f4face7ec878e3bf2ca890a3a
shimeoki <shimeoki@gmail.com>
2024-11-23 10:42:24 +0300

    feat(obj/reader): add ObjModelReader

---
+++ lib/src/main/java/com/github/shimeoki/jshaper/obj/reader/ObjModelReader.java
@@ -0,0 +1,14 @@
+package com.github.shimeoki.jshaper.obj.reader;
+
+import java.io.File;
+
+import com.github.shimeoki.jshaper.obj.data.ObjFile;
+
+public final class ObjModelReader implements ObjReader {
+
+    @Override
+    public ObjFile read(final File f) {
+        // TODO
+        return null;
+    }
+}
