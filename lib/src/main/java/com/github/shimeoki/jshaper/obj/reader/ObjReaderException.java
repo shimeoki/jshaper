ed4dd276c47c7ea5b5b014f4481b341188043407
shimeoki <shimeoki@gmail.com>
2024-11-24 16:40:45 +0300

    refactor(ObjReaderException): change err

--- lib/src/main/java/com/github/shimeoki/jshaper/obj/reader/ObjReaderException.java
+++ lib/src/main/java/com/github/shimeoki/jshaper/obj/reader/ObjReaderException.java
@@ -2,11 +2,11 @@ package com.github.shimeoki.jshaper.obj.reader;
 
 public class ObjReaderException extends Exception {
 
-    private static final String err = "ObjReader: ";
+    private static final String err = "ObjReader";
     private final ObjReaderExceptionType type;
 
     public ObjReaderException(final ObjReaderExceptionType type, final String msg) {
-        super(String.format("%s %s %s", err, type, msg));
+        super(String.format("%s: %s %s", err, type, msg));
         this.type = type;
     }
 
