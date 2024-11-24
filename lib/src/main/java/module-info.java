65597d61d08f79e020b20c649de5116c334a8e07
shimeoki <shimeoki@gmail.com>
2024-11-24 21:39:38 +0300

    build: add missing export

--- lib/src/main/java/module-info.java
+++ lib/src/main/java/module-info.java
@@ -5,7 +5,8 @@ module com.github.shimeoki.jshaper {
     exports com.github.shimeoki.jshaper.geom;
 
     // obj
+    exports com.github.shimeoki.jshaper.obj;
+    exports com.github.shimeoki.jshaper.obj.reader;
     exports com.github.shimeoki.jshaper.obj.data;
     exports com.github.shimeoki.jshaper.obj.geom;
-    exports com.github.shimeoki.jshaper.obj.reader;
 }
