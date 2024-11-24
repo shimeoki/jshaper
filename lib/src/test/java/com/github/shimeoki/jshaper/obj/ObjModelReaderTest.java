ba8ad8f1695f27e18aa0ebb4f1fc3a9675391af5
shimeoki <shimeoki@gmail.com>
2024-11-24 20:22:58 +0300

    test: add case1()
    rename case1 filename, because the filename with spaces failed.
    
    try to fix this issue in the future

--- lib/src/test/java/com/github/shimeoki/jshaper/obj/ObjModelReaderTest.java
+++ lib/src/test/java/com/github/shimeoki/jshaper/obj/ObjModelReaderTest.java
@@ -1,11 +1,44 @@
 package com.github.shimeoki.jshaper.obj;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
 import java.io.File;
 
+import org.junit.jupiter.api.Test;
+
+import com.github.shimeoki.jshaper.obj.data.ObjFile;
+import com.github.shimeoki.jshaper.obj.data.ObjVertexData;
+import com.github.shimeoki.jshaper.obj.reader.ObjReaderException;
+
 public final class ObjModelReaderTest {
+
+    private final ObjModelReader reader = new ObjModelReader();
 
     private File file(final String filename) {
         final String path = getClass().getResource(filename).getPath();
         return new File(path);
+    }
+
+    @Test
+    public void case1() {
+        final File file = file("001/cnc.obj");
+
+        ObjFile f = null;
+        try {
+            f = reader.read(file);
+        } catch (final ObjReaderException e) {
+            fail(e.getMessage());
+        }
+
+        final ObjVertexData vertexData = f.vertexData();
+        assertNotNull(vertexData);
+
+        assertEquals(15789, vertexData.vertices().size());
+        assertEquals(28209, vertexData.textureVertices().size());
+        assertEquals(26181, vertexData.vertexNormals().size());
+        assertEquals(31930, f.elements().faces().size());
+        assertEquals(30, f.groupingData().groupNames().size());
     }
 }
