5bcdc3ad8d8411ccbe867f6edc4e6517e4bbdd7b
shimeoki <shimeoki@gmail.com>
2024-11-24 17:16:38 +0300

    build: export module packages

--- lib/src/main/java/module-info.java
+++ lib/src/main/java/module-info.java
@@ -1,3 +1,11 @@
 module com.github.shimeoki.jshaper {
 
+    // shared
+    exports com.github.shimeoki.jshaper.data;
+    exports com.github.shimeoki.jshaper.geom;
+
+    // obj
+    exports com.github.shimeoki.jshaper.obj.data;
+    exports com.github.shimeoki.jshaper.obj.geom;
+    exports com.github.shimeoki.jshaper.obj.reader;
 }
