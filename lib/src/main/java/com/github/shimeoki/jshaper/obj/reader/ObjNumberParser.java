38493c6bbba2286bf2f19219bcf5a5b12464ff4a
shimeoki <shimeoki@gmail.com>
2024-11-25 12:17:22 +0300

    feat(obj/reader): add ObjNumberParser
    fix(ObjNumberParser): access modifiers

---
+++ lib/src/main/java/com/github/shimeoki/jshaper/obj/reader/ObjNumberParser.java
@@ -0,0 +1,20 @@
+package com.github.shimeoki.jshaper.obj.reader;
+
+public final class ObjNumberParser {
+
+    public static float parseFloat(final String s) throws ObjReaderException {
+        try {
+            return Float.parseFloat(s);
+        } catch (final NumberFormatException e) {
+            throw new ObjReaderException(ObjReaderExceptionType.PARSE, "invalid float format");
+        }
+    }
+
+    public static int parseInt(final String s) throws ObjReaderException {
+        try {
+            return Integer.parseInt(s);
+        } catch (final NumberFormatException e) {
+            throw new ObjReaderException(ObjReaderExceptionType.PARSE, "invalid int format");
+        }
+    }
+}
