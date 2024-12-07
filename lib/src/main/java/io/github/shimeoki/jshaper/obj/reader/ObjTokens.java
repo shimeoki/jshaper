85a2c967205353aac5ad65465b5dc8928537b95a
shimeoki <shimeoki@gmail.com>
2024-12-07 10:43:43 +0300

    feat(obj/reader): add ObjTokens

---
+++ lib/src/main/java/io/github/shimeoki/jshaper/obj/reader/ObjTokens.java
@@ -0,0 +1,24 @@
+package io.github.shimeoki.jshaper.obj.reader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class ObjTokens {
+
+    private final List<ObjTokenized> tokens = new ArrayList<>();
+
+    public ObjTokens() {
+    }
+
+    public boolean empty() {
+        return tokens.isEmpty();
+    }
+
+    public ObjToken lineToken() {
+        if (empty()) {
+            return null;
+        } else {
+            return tokens.getFirst().token();
+        }
+    }
+}
