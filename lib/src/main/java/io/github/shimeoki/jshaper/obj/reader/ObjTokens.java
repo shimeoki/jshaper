4da8e605c41cb804adc0bb6ab1fa1397e8fce947
shimeoki <shimeoki@gmail.com>
2024-12-07 10:50:03 +0300

    feat(ObjTokens): add more methods

--- lib/src/main/java/io/github/shimeoki/jshaper/obj/reader/ObjTokens.java
+++ lib/src/main/java/io/github/shimeoki/jshaper/obj/reader/ObjTokens.java
@@ -2,6 +2,7 @@ package io.github.shimeoki.jshaper.obj.reader;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public final class ObjTokens {
 
@@ -23,6 +24,48 @@ public final class ObjTokens {
             return null;
         } else {
             return tokens.getFirst().token();
+        }
+    }
+
+    public void add(final ObjTokenized token) {
+        tokens.add(Objects.requireNonNull(token));
+    }
+
+    public ObjTokenized tokenized(final int index) {
+        if (!validIndex(index)) {
+            throw new IllegalArgumentException("invalid index");
+        }
+
+        return tokens.get(index);
+    }
+
+    public ObjToken token(final int index) {
+        if (!validIndex(index)) {
+            throw new IllegalArgumentException("invalid index");
+        }
+
+        return tokens.get(index).token();
+    }
+
+    public String value(final int index) {
+        if (!validIndex(index)) {
+            throw new IllegalArgumentException("invalid index");
+        }
+
+        return tokens.get(index).value();
+    }
+
+    public int size() {
+        return tokens.size();
+    }
+
+    public boolean validIndex(final int index) {
+        if (index < 0) {
+            return false;
+        } else if (index >= size()) {
+            return false;
+        } else {
+            return true;
         }
     }
 }
