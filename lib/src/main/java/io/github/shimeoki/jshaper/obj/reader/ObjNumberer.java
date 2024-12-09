fef32bc0175c954c45b0e11aff9ff7d7d0c9a707
shimeoki <shimeoki@gmail.com>
2024-12-10 02:09:17 +0300

    fix(ObjNumberer): requireNonNull()

--- lib/src/main/java/io/github/shimeoki/jshaper/obj/reader/ObjNumberer.java
+++ lib/src/main/java/io/github/shimeoki/jshaper/obj/reader/ObjNumberer.java
@@ -1,20 +1,24 @@
 package io.github.shimeoki.jshaper.obj.reader;
 
+import java.util.Objects;
+
 public final class ObjNumberer {
 
     public static float parseFloat(final String s) throws ObjReaderException {
         try {
-            return Float.parseFloat(s);
+            return Float.parseFloat(Objects.requireNonNull(s));
         } catch (final NumberFormatException e) {
-            throw new ObjReaderException(ObjReaderExceptionType.PARSE, "invalid float format");
+            throw new ObjReaderException(ObjReaderExceptionType.PARSE,
+                    "invalid float format");
         }
     }
 
     public static int parseInt(final String s) throws ObjReaderException {
         try {
-            return Integer.parseInt(s);
+            return Integer.parseInt(Objects.requireNonNull(s));
         } catch (final NumberFormatException e) {
-            throw new ObjReaderException(ObjReaderExceptionType.PARSE, "invalid int format");
+            throw new ObjReaderException(ObjReaderExceptionType.PARSE,
+                    "invalid int format");
         }
     }
 }
