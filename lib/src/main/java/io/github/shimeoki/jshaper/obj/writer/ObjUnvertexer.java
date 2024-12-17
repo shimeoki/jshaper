d3dcbdae43fa234b3a603881bf54e82cbb18fa54
shimeoki <shimeoki@gmail.com>
2024-12-17 14:24:05 +0300

    feat(ObjUnvertexer): add method signatures

--- lib/src/main/java/io/github/shimeoki/jshaper/obj/writer/ObjUnvertexer.java
+++ lib/src/main/java/io/github/shimeoki/jshaper/obj/writer/ObjUnvertexer.java
@@ -1,12 +1,24 @@
 package io.github.shimeoki.jshaper.obj.writer;
 
+import io.github.shimeoki.jshaper.obj.geom.ObjTextureVertex;
 import io.github.shimeoki.jshaper.obj.geom.ObjVertex;
+import io.github.shimeoki.jshaper.obj.geom.ObjVertexNormal;
 
 public final class ObjUnvertexer {
 
     private final StringBuilder builder = new StringBuilder();
 
     public String parseVertex(final ObjVertex v) {
+        // TODO
+        return null;
+    }
+
+    public String parseTextureVertex(final ObjTextureVertex vt) {
+        // TODO
+        return null;
+    }
+
+    public String parseVertexNormal(final ObjVertexNormal vn) {
         // TODO
         return null;
     }
