2f353d5b94b4ac88ea0017884ead5350d188c6e4
shimeoki <shimeoki@gmail.com>
2024-12-17 13:44:55 +0300

    feat: add ObjModelWriter

---
+++ lib/src/main/java/io/github/shimeoki/jshaper/obj/ObjModelWriter.java
@@ -0,0 +1,13 @@
+package io.github.shimeoki.jshaper.obj;
+
+import java.io.File;
+
+import io.github.shimeoki.jshaper.obj.data.ObjFile;
+
+public final class ObjModelWriter implements ObjWriter {
+
+    @Override
+    public void write(final ObjFile src, final File dst) {
+
+    }
+}
