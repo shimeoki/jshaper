87abc3c287516c5f4863ff04592afa15369e1d42
shimeoki <shimeoki@gmail.com>
2024-12-17 14:21:39 +0300

    feat: add ObjUnvertexer (WIP)

---
+++ lib/src/main/java/io/github/shimeoki/jshaper/obj/writer/ObjUnvertexer.java
@@ -0,0 +1,13 @@
+package io.github.shimeoki.jshaper.obj.writer;
+
+import io.github.shimeoki.jshaper.obj.geom.ObjVertex;
+
+public final class ObjUnvertexer {
+
+    private final StringBuilder builder = new StringBuilder();
+
+    public String parseVertex(final ObjVertex v) {
+        // TODO
+        return null;
+    }
+}
