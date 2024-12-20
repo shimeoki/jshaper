a8215d7c85c63ec6d2eb41226e509971598ac68a
shimeoki <shimeoki@gmail.com>
2024-12-20 11:49:41 +0300

    feat: add initial model writer fields

--- lib/src/main/java/io/github/shimeoki/jshaper/obj/ObjModelWriter.java
+++ lib/src/main/java/io/github/shimeoki/jshaper/obj/ObjModelWriter.java
@@ -1,10 +1,30 @@
 package io.github.shimeoki.jshaper.obj;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import io.github.shimeoki.jshaper.obj.data.ObjFile;
+import io.github.shimeoki.jshaper.obj.geom.ObjTextureVertex;
+import io.github.shimeoki.jshaper.obj.geom.ObjVertex;
+import io.github.shimeoki.jshaper.obj.geom.ObjVertexNormal;
 
 public final class ObjModelWriter implements ObjWriter {
+
+    private ObjFile src;
+    private File dst;
+
+    private List<ObjVertex> vertices;
+    private List<ObjTextureVertex> textureVertices;
+    private List<ObjVertexNormal> vertexNormals;
+
+    private final Map<ObjVertex, Integer> vertexIndices = new HashMap<>();
+    private final Map<ObjTextureVertex, Integer> textureVertexIndices = new HashMap<>();
+    private final Map<ObjVertexNormal, Integer> vertexNormalIndices = new HashMap<>();
+
+    public ObjModelWriter() {
+    }
 
     @Override
     public void write(final ObjFile src, final File dst) {
