ac57f802c15551a829f5636b50eff8508f5c0f25
shimeoki <shimeoki@gmail.com>
2024-12-23 17:08:02 +0300

    feat(ObjUnvertexer): implement parseVertex()

--- lib/src/main/java/io/github/shimeoki/jshaper/obj/writer/ObjUnvertexer.java
+++ lib/src/main/java/io/github/shimeoki/jshaper/obj/writer/ObjUnvertexer.java
@@ -3,14 +3,30 @@ package io.github.shimeoki.jshaper.obj.writer;
 import io.github.shimeoki.jshaper.obj.geom.ObjTextureVertex;
 import io.github.shimeoki.jshaper.obj.geom.ObjVertex;
 import io.github.shimeoki.jshaper.obj.geom.ObjVertexNormal;
+import io.github.shimeoki.jshaper.obj.reader.ObjTokenizer;
 
 public final class ObjUnvertexer {
 
     private final StringBuilder builder = new StringBuilder();
 
     public String parseVertex(final ObjVertex v) {
-        // TODO
-        return null;
+        builder.setLength(0);
+
+        builder.append(ObjTokenizer.VERTEX);
+
+        builder.append(' ');
+        builder.append(v.x());
+
+        builder.append(' ');
+        builder.append(v.y());
+
+        builder.append(' ');
+        builder.append(v.z());
+
+        builder.append(' ');
+        builder.append(v.w());
+
+        return builder.toString();
     }
 
     public String parseTextureVertex(final ObjTextureVertex vt) {
