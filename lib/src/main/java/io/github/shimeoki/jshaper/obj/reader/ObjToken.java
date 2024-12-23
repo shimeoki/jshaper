fadfa4cfaff1e0f8c3c7b10b3208c6478490ea59
shimeoki <shimeoki@gmail.com>
2024-12-23 18:18:52 +0300

    refactor(ObjToken): don't use ObjTokenizer

--- lib/src/main/java/io/github/shimeoki/jshaper/obj/reader/ObjToken.java
+++ lib/src/main/java/io/github/shimeoki/jshaper/obj/reader/ObjToken.java
@@ -5,13 +5,13 @@ import java.util.Objects;
 public enum ObjToken {
 
     NIL(""),
-    COMMENT(ObjTokenizer.COMMENT),
-    VERTEX(ObjTokenizer.VERTEX),
-    TEXTURE_VERTEX(ObjTokenizer.TEXTURE_VERTEX),
-    VERTEX_NORMAL(ObjTokenizer.VERTEX_NORMAL),
-    PARAMETER_SPACE_VERTEX(ObjTokenizer.PARAMETER_SPACE_VERTEX),
-    FACE(ObjTokenizer.FACE),
-    GROUP_NAME(ObjTokenizer.GROUP_NAME);
+    COMMENT("#"),
+    VERTEX("v"),
+    TEXTURE_VERTEX("vt"),
+    VERTEX_NORMAL("vn"),
+    PARAMETER_SPACE_VERTEX("vp"),
+    FACE("f"),
+    GROUP_NAME("g");
 
     private final String txt;
 
