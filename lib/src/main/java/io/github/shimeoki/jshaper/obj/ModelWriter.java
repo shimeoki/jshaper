f2ecf63d6a279bec5f27962b7d2756b5bbb3ca78
shimeoki <shimeoki@gmail.com>
2024-12-29 01:30:06 +0300

    feat: write vertex data

--- lib/src/main/java/io/github/shimeoki/jshaper/obj/ModelWriter.java
+++ lib/src/main/java/io/github/shimeoki/jshaper/obj/ModelWriter.java
@@ -9,6 +9,8 @@ import java.util.List;
 import java.util.Map;
 
 import io.github.shimeoki.jshaper.ObjFile;
+import io.github.shimeoki.jshaper.ShaperError;
+import io.github.shimeoki.jshaper.obj.writer.Unvertexer;
 
 public final class ModelWriter implements Writer {
 
@@ -16,6 +18,8 @@ public final class ModelWriter implements Writer {
     private File dst;
 
     private BufferedWriter writer;
+
+    private Unvertexer unvertexer;
 
     private List<Vertex> vertices;
     private List<TextureVertex> textureVertices;
@@ -25,11 +29,14 @@ public final class ModelWriter implements Writer {
     private final Map<TextureVertex, Integer> textureVertexIndices = new HashMap<>();
     private final Map<VertexNormal, Integer> vertexNormalIndices = new HashMap<>();
 
+    private int vertexDataIndex;
+
     public ModelWriter() {
     }
 
     private void open(final File f) {
         openWriter(f);
+        unvertexer = new Unvertexer();
     }
 
     private void openWriter(final File f) {
@@ -44,5 +51,46 @@ public final class ModelWriter implements Writer {
     @Override
     public void write(final ObjFile src, final File dst) {
         // TODO
+    }
+
+    private void writeVertexData() throws ShaperError {
+        try {
+            writeVertices();
+            writeTextureVertices();
+            writeVertexNormals();
+        } catch (final IOException e) {
+            throw new ShaperError(ShaperError.Type.IO,
+                    "io error occured while writing the vertex data");
+        }
+    }
+
+    private void writeVertices() throws IOException {
+        vertexDataIndex = 1;
+
+        for (final Vertex v : vertices) {
+            writer.write(unvertexer.parseVertex(v));
+            writer.newLine();
+            vertexIndices.put(v, vertexDataIndex);
+        }
+    }
+
+    private void writeTextureVertices() throws IOException {
+        vertexDataIndex = 1;
+
+        for (final TextureVertex vt : textureVertices) {
+            writer.write(unvertexer.parseTextureVertex(vt));
+            writer.newLine();
+            textureVertexIndices.put(vt, vertexDataIndex);
+        }
+    }
+
+    private void writeVertexNormals() throws IOException {
+        vertexDataIndex = 1;
+
+        for (final VertexNormal vn : vertexNormals) {
+            writer.write(unvertexer.parseVertexNormal(vn));
+            writer.newLine();
+            vertexNormalIndices.put(vn, vertexDataIndex);
+        }
     }
 }
