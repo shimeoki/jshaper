c335490be3a1cc13f2385cd5a066545594396fce
shimeoki <shimeoki@gmail.com>
2024-12-30 23:21:01 +0300

    refactor!: add Commenter and use it

---
+++ lib/src/main/java/io/github/shimeoki/jshaper/obj/writer/Commenter.java
@@ -0,0 +1,23 @@
+package io.github.shimeoki.jshaper.obj.writer;
+
+import java.util.Objects;
+
+import io.github.shimeoki.jshaper.obj.Token;
+
+public class Commenter {
+
+    private final StringBuilder builder = new StringBuilder();
+
+    public Commenter() {
+    }
+
+    public String parse(final String comment) {
+        builder.setLength(0);
+
+        builder.append(Token.COMMENT);
+        builder.append(' ');
+        builder.append(Objects.requireNonNull(comment));
+
+        return builder.toString();
+    }
+}
